2d7754362c872f3e96112074b510d1fe63831f75
dongweiweiFENG <124340386+dongweiweiFENG@users.noreply.github.com>
2023-03-02 13:30:50 +0800

    Add files via upload

---
+++ SIMP.m
@@ -0,0 +1,10 @@
+function [x,dx]=SIMP(xPhys,p)
+indx1=xPhys-1>=0;
+indx2=xPhys-1<0;
+x(indx1)=xPhys(indx1);
+x(indx2)=xPhys(indx2).^p;
+dx(indx1)=1;
+dx(indx2)=p*xPhys(indx2).^(p-1);
+x=x';
+dx=dx';
+end
