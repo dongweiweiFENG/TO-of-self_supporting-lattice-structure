bcabde1a1711e1f348e79e1b16a1140f5b88f0c0
dongweiweiFENG <124340386+dongweiweiFENG@users.noreply.github.com>
2023-03-02 15:48:24 +0800

    Update scaling_fun.m

--- scaling_fun.m
+++ scaling_fun.m
@@ -1,3 +1,4 @@
+% size filter for simplification
 function [x,dx]=scaling_fun(xPhys,aa,bb)
 indx1=xPhys-aa>=0;
 indx2=xPhys-aa<0;
@@ -8,8 +9,4 @@ dx(indx2)=1-tanh(bb*(xPhys(indx2)-aa(indx2))).*tanh(bb*(xPhys(indx2)-aa(indx2)))
     xPhys(indx2).*(-2*bb*tanh(bb.*(xPhys(indx2)-aa(indx2)))).*(1-tanh(bb.*(xPhys(indx2)-aa(indx2))).*tanh(bb.*(xPhys(indx2)-aa(indx2))));
 x=x';
 dx=dx';
-% x2(indx1)=xPhys(indx1);
-% eps=1e-6;
-% x2(indx2)=(xPhys(indx2)+eps).*(1-tanh(bb.*((xPhys(indx2)+eps)-aa(indx2))).*tanh(bb.*((xPhys(indx2)+eps)-aa(indx2))));
-% dx2=(x2'-x)/eps;
 end
